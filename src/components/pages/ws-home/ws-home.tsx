f9f44d738ac37c70ec5a5cbde86f54300f035717
Nils Benz <nils.benz@adcubum.com>
2019-07-04 20:04:03 +0200

    text auf homeseite

--- src/components/pages/ws-home/ws-home.tsx
+++ src/components/pages/ws-home/ws-home.tsx
@@ -11,6 +11,7 @@ export class WsHome {
   render() {
     return (
       <div>
+        <ws-text class="text">Ich bin Nils, 19 Jahre jung und Hobby Foto- und Videograf. Neben meinem Job als Informatiker mache ich in meiner Freizeit Videos für Hochzeitspaare, kleine Unternehmen und weitere. Gefallen dir meine Fotos? Man kann in diesem Online Shop Lizenzen dafür erwerben und sie dann nach Belieben weiterverwenden.</ws-text>
         <img src="/assets/images/home.jpg" />
       </div>
     );
